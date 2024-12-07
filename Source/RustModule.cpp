1ecd435028800f70c58fdbc85e9196dfe381b3e5
BeastLe9enD <beastle9end@outlook.de>
2024-12-07 22:16:52 +0100

    ✨ Add rust module startup namespace

--- Source/RustModule.cpp
+++ Source/RustModule.cpp
@@ -1,1 +1,17 @@
 #include "Bridge.cpp"
+
+extern "C" {
+    void ur_register_module();
+}
+
+namespace RustModule {
+    void Init() {
+        UE_LOG(LogTemp, Warning, TEXT("FRustPluginModule::StartupModule"));
+
+        ur_register_module();
+    }
+
+    void Shutdown() {
+        UE_LOG(LogTemp, Warning, TEXT("FRustPluginModule::ShutdownModule"));
+    }
+}
---
+++ Source/RustModule.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+namespace RustModule {
+    void Init();
+    void Shutdown();
+}
