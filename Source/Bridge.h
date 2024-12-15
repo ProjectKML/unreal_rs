7e261cb9a516b853bdf53439a077ac592fa931ef
BeastLe9enD <beastle9end@outlook.de>
2024-12-15 22:46:07 +0100

    Update Bridge.h

--- Source/Bridge.h
+++ Source/Bridge.h
@@ -1,9 +1,12 @@
 #pragma once
 
-#define DEFINE_HANDLE(name) typedef struct {} name;
+#define DEFINE_HANDLE(name) typedef void* name;
 
 #ifdef __cplusplus
 extern "C" {
+#else
+#include <stdint.h>
+#include <string.h>
 #endif
     void ur_log(const char* s, size_t len);
 #ifdef __cplusplus
